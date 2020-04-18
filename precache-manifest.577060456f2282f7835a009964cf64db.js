bdb735ee422381e3638e80d71c6e6046e909ceea
Alexandr-Paleev <34216826+Alexandr-Paleev@users.noreply.github.com>
2020-04-18 11:00:46 +0300

    Updates

---
+++ precache-manifest.577060456f2282f7835a009964cf64db.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b30d33091e0fdf794c3cc1f5bcca694",
+    "url": "/React-site/index.html"
+  },
+  {
+    "revision": "9c90c87e3c1cb137fbc0",
+    "url": "/React-site/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "6a8e46c3af62da2ce346",
+    "url": "/React-site/static/css/main.e54c918f.chunk.css"
+  },
+  {
+    "revision": "9c90c87e3c1cb137fbc0",
+    "url": "/React-site/static/js/2.fff35963.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-site/static/js/2.fff35963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8e46c3af62da2ce346",
+    "url": "/React-site/static/js/main.47320b35.chunk.js"
+  },
+  {
+    "revision": "563b0319e0b00827dab4",
+    "url": "/React-site/static/js/runtime-main.e8d2bd34.js"
+  },
+  {
+    "revision": "23090ba59e27ed71bbb569667fb7208e",
+    "url": "/React-site/static/media/about01.23090ba5.jpg"
+  },
+  {
+    "revision": "79303be178ed014704d2f3495a30dbd7",
+    "url": "/React-site/static/media/about02.79303be1.jpg"
+  },
+  {
+    "revision": "4d3e532bbe710c627c547de13df69a2d",
+    "url": "/React-site/static/media/about03.4d3e532b.jpg"
+  },
+  {
+    "revision": "87a2882d577ce4e8e74c0dddb4d89c44",
+    "url": "/React-site/static/media/about04.87a2882d.jpg"
+  },
+  {
+    "revision": "5caf450c00a621124d5b82e4c47573a0",
+    "url": "/React-site/static/media/about05.5caf450c.jpg"
+  },
+  {
+    "revision": "74e3859d32a713e55de3d2c12b92e9c6",
+    "url": "/React-site/static/media/blogImg.74e3859d.gif"
+  },
+  {
+    "revision": "86706d4fee6cb6ff3e670ad715b34434",
+    "url": "/React-site/static/media/first.86706d4f.jpg"
+  },
+  {
+    "revision": "f79c7cb14d612e896c5c2e61f96d3e8f",
+    "url": "/React-site/static/media/second.f79c7cb1.jpg"
+  },
+  {
+    "revision": "8125008d0552c279d013b640cfde2139",
+    "url": "/React-site/static/media/third.8125008d.jpg"
+  }
+]);
